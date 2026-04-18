0efa0bcecc7df4f1a13fa7d67092ff38d4652117
Fllavex <mashtalermaksim2012@gmail.com>
2026-04-18 14:43:40 +0300

    Добавив відображення карток майже повністю зробив меню

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
             <wa-card class="card-overview">
   <img
     slot="media"
-    src="https://images.unsplash.com/photo-1559209172-0ff8f6d49ff7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
-    alt="A kitten sits patiently between a terracotta pot and decorative grasses."
+    src="${product.image}"
+    alt="${product.name}"
   />
 
-  <strong>Mittens</strong><br />
-  This kitten is as cute as he is playful. Bring him home today!<br />
-  <small class="wa-caption-s">6 weeks old</small>
+  <strong>${product.name}</strong><br />
+  ${product.description}<br />
+  <small class="wa-caption-s">$${product.price}</small>
 
-  <wa-button slot="footer" variant="brand" pill>More Info</wa-button>
+  <wa-button slot="footer" variant="brand" pill>Купити</wa-button>
   <wa-rating slot="footer-actions" label="Rating"></wa-rating>
 </wa-card>
 
